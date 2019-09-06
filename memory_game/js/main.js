31c41a27e1287a8561cefcdbd29c0227c273fbab
Robert Marshall Sykes <Marshall@Roberts-MacBook-Pro.local>
2019-09-06 14:23:52 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,51 @@
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+const cardsInPlay = [];
+
+
+
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		console.log("You found a match!");
+	} else {
+  		console.log("Sorry, try again.");
+	}
+}
+
+
+function flipCard(cardId) {
+	
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].suit);
+	console.log("User flipped " + cards[cardId].cardImage);
+
+	cardsInPlay.push(cards[cardId].rank);
+}
+
+
+flipCard(2);
+flipCard(0);
+
+checkForMatch();
